4ac29ccc7a89ce0495a49e77054f209de254ec0e
Muhammed Abidh <abidh.k@appsintegra.com>
2019-11-16 22:42:10 +0400

    scaling and optimization

--- example/src/App.js
+++ example/src/App.js
@@ -1,20 +1,52 @@
-import React, { Component } from 'react'
+import React from "react";
+import Document from "react-pdf-preview";
+import { pdfBase64 } from "./pdfBase64";
 
-import Document from 'react-pdf-preview'
-import { pdfBase64 } from './pdfBase64'
+export default function App() {
+  const pdf = pdfBase64;
+  const [page, setPage] = React.useState(1);
+  const [scale, setScale] = React.useState(1);
+  const [numPages, setNumPages] = React.useState(null);
 
-export default class App extends Component {
-  render () {
+  const onSuccessLoad = data => {
+    setNumPages(data._pdfInfo.numPages);
+  };
 
-    const pdf = pdfBase64;    
-    const onSuccessLoad = (data) => {
-      console.log(data)
-    }
-    
-    return (
-      <div>
-        <Document pdf={pdf} pageNum={1} onLoad={onSuccessLoad} />
-      </div>
-    )
-  }
+  return (
+    <div>
+      <Document pdf={pdf} scale={scale} pageNum={page} onLoad={onSuccessLoad} />
+      
+      <button
+        onClick={() => {
+          page > 1 && setPage(page - 1);
+        }}
+      >
+        prev
+      </button>
+
+      <button
+        onClick={() => {
+          page < numPages && setPage(page + 1);
+        }}
+      >
+        next
+      </button>
+
+      <button
+        onClick={() => {
+          setScale(scale + 1);
+        }}
+      >
+        scale +
+      </button>
+
+      <button
+        onClick={() => {
+          setScale(scale - 1);
+        }}
+      >
+        scale -
+      </button>
+    </div>
+  );
 }
--- src/index.js
+++ src/index.js
@@ -1,83 +1,86 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-var pdfjsLib
+import React, { useRef } from "react";
 
-const Document = ({pdf, onLoad, pageNum = 1}) => {
-  const [libLoaded, setLibLoaded] = React.useState(false)
+let pdfjsLib;
+
+const Document = ({ pdf, onLoad, pageNum = 1, scale = 1 }) => {
+  const [libLoaded, setLibLoaded] = React.useState(false);
   React.useEffect(() => {
     function addScript(attribute, text, callback) {
-      var s = document.createElement('script')
-      for (var attr in attribute) {
-        s.setAttribute(attr, attribute[attr] ? attribute[attr] : null)
+      const s = document.createElement("script");
+      for (const attr in attribute) {
+        s.setAttribute(attr, attribute[attr] ? attribute[attr] : null);
       }
-      s.innerHTML = text
-      s.onload = callback
-      document.body.appendChild(s)
+      s.innerHTML = text;
+      s.onload = callback;
+      document.body.appendChild(s);
     }
 
-    addScript({
-      src: 'https://mozilla.github.io/pdf.js/build/pdf.js',
-      type: 'text/javascript',
-      async: null
-    }, '<div>innerHTML</div>', function() {
-      if (!pdfjsLib) {
-        pdfjsLib = window['pdfjs-dist/build/pdf']
-        pdfjsLib.GlobalWorkerOptions.workerSrc = 'https://gitcdn.xyz/repo/mozilla/pdf.js/gh-pages/build/pdf.worker.js'
+    addScript(
+      {
+        src: "https://mozilla.github.io/pdf.js/build/pdf.js",
+        type: "text/javascript",
+        async: null
+      },
+      "<div>innerHTML</div>",
+      function() {
+        if (!pdfjsLib) {
+          pdfjsLib = window["pdfjs-dist/build/pdf"];
+          pdfjsLib.GlobalWorkerOptions.workerSrc =
+            "https://gitcdn.xyz/repo/mozilla/pdf.js/gh-pages/build/pdf.worker.js";
+        }
+        setLibLoaded(true);
       }
-      setLibLoaded(true)
-    })
-  }, [])
+    );
+  }, []);
+
+  const pdfV = React.useMemo(() => {
+    return (
+      <PdfViewer pdf={pdf} scale={scale} callback={onLoad} pageNum={pageNum} />
+    );
+  }, [pageNum, scale]);
 
   return (
-    <div className='container'>
-      { libLoaded ? <PdfViewer pdf={pdf} callback={onLoad} pageNum={pageNum} /> : <div className='loading'><p className='loading_indicator'>Loading...</p></div>}
+    <div className="container">
+      {libLoaded ? (
+        pdfV
+      ) : (
+        <div className="loading">
+          <p className="loading_indicator">Loading...</p>
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
-Document.propTypes = {
-  pdf: PropTypes.string.isRequired,
-  onLoad: PropTypes.func,
-  pageNum: PropTypes.number
-}
+export default Document;
 
-export default Document
-
-const PdfViewer = ({pdf, pageNum, callback}) => {
+const PdfViewer = ({ pdf, pageNum, callback, scale }) => {
   // eslint-disable-next-line no-undef
-  const pdfData = atob(pdf)
+  const pdfData = atob(pdf);
+  const renderCanvas = useRef(null);
 
-  const loadingTask = pdfjsLib.getDocument({ data: pdfData })
-  loadingTask.promise.then(function (pdf) {
-    callback(pdf)
-    pdf.getPage(pageNum).then(function (_page) {
-      const scale = 1
-      const viewport = _page.getViewport({ scale: scale })
+  const loadingTask = pdfjsLib.getDocument({ data: pdfData });
+  loadingTask.promise.then(
+    function(pdf) {
+      callback(pdf);
+      pdf.getPage(pageNum).then(function(_page) {
+        const viewport = _page.getViewport({ scale });
+        const canvas = renderCanvas.current;
+        const context = canvas.getContext("2d");
+        canvas.height = viewport.height;
+        canvas.width = viewport.width;
+        const renderContext = {
+          canvasContext: context,
+          viewport
+        };
+        const renderTask = _page.render(renderContext);
+        renderTask.promise.then(function() {});
+      });
+    },
+    function(reason) {
+      console.error(reason);
+    }
+  );
 
-      const canvas = document.getElementById('the-canvas')
-      const context = canvas.getContext('2d')
-      canvas.height = viewport.height
-      canvas.width = viewport.width
-
-      const renderContext = {
-        canvasContext: context,
-        viewport: viewport
-      }
-      const renderTask = _page.render(renderContext)
-      renderTask.promise.then(function () {
-      })
-    })
-  }, function (reason) {
-    console.error(reason)
-  })
-
-  return (
-    <canvas id='the-canvas' className='pdf_canvas' />
-  )
-}
-
-PdfViewer.propTypes = {
-  pdf: PropTypes.string.isRequired,
-  callback: PropTypes.func,
-  pageNum: PropTypes.number
-}
+  return <canvas ref={renderCanvas} className="pdf_canvas" />;
+};
